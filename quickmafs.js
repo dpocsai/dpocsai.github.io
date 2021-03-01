83c2f3ebb821eff5d7afc0d7f064d372a0741f00
dpocsai <75903729+dpocsai@users.noreply.github.com>
2021-03-01 14:58:17 -0500

    Add files via upload

---
+++ quickmafs.js
@@ -0,0 +1,57 @@
+const boxes = document.querySelectorAll('div');
+let x = [];
+let records = [];
+let nums = [];
+while (nums.length < 18) {
+	nums.push(Math.floor(Math.random() * 10));
+}
+for (let i = 0; i < boxes.length; i++) {
+	const plus = document.querySelector('#plus');
+	const times = document.querySelector('#times');
+	plus.addEventListener('click', function() {
+		const s = new Date() / 1000;
+		boxes[i].innerText = `${Math.floor(Math.random() * 10)} + ${Math.floor(Math.random() * 10)}`;
+		x.push(+boxes[i].innerText[0] + +boxes[i].innerText[4]);
+		x.sort((a, b) => a - b);
+		boxes[i].addEventListener('click', function() {
+			if (+boxes[i].innerText[0] + +boxes[i].innerText[4] !== Math.min(...x)) {
+				document.body.style.backgroundColor = 'maroon';
+				const h1 = document.querySelector('h1');
+				h1.innerText = 'GAME OVER';
+			} else if (+boxes[i].innerText[0] + +boxes[i].innerText[4] === Math.min(...x)) {
+				x.shift();
+				boxes[i].style.backgroundColor = 'lightgray';
+				boxes[i].style.color = 'lightgray';
+				if (!x.length) {
+					const h3 = document.querySelector('h3');
+					const f = new Date() / 1000;
+					const time = f - s;
+					h3.innerText = `TIME: ${time}`;
+				}
+			}
+		});
+	});
+	times.addEventListener('click', function() {
+		const s = new Date() / 1000;
+		boxes[i].innerText = `${Math.floor(Math.random() * 9) + 1} x ${Math.floor(Math.random() * 9) + 1}`;
+		x.push(+boxes[i].innerText[0] * +boxes[i].innerText[4]);
+		x.sort((a, b) => a - b);
+		boxes[i].addEventListener('click', function() {
+			if (+boxes[i].innerText[0] * +boxes[i].innerText[4] !== Math.min(...x)) {
+				document.body.style.backgroundColor = 'maroon';
+				const h1 = document.querySelector('h1');
+				h1.innerText = 'GAME OVER';
+			} else if (+boxes[i].innerText[0] * +boxes[i].innerText[4] === Math.min(...x)) {
+				x.shift();
+				boxes[i].style.backgroundColor = 'lightgray';
+				boxes[i].style.color = 'lightgray';
+				if (!x.length) {
+					const h3 = document.querySelector('h3');
+					const f = new Date() / 1000;
+					const time = f - s;
+					h3.innerText = `${time}`;
+				}
+			}
+		});
+	});
+}
